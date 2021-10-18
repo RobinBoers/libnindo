99846df259d6227214b5b3164146e9a8a69f4be4
Robin Boers <robindev2019@outlook.com>
2021-10-18 17:16:59 +0200

    trim spaces from username and password

--- lib/nindo/accounts.ex
+++ lib/nindo/accounts.ex
@@ -6,6 +6,9 @@ defmodule Nindo.Accounts do
   import Nindo.Core
 
   def new(username, password, email) do
+    username = String.trim username
+    password = String.trim password
+
     salt = Auth.get_salt()
     password = Auth.hash_pass(password, salt)
 
@@ -38,8 +41,18 @@ defmodule Nindo.Accounts do
   # Private methods
 
   defp check_login(username, password) do
+    case user_exists(username) do
+      true -> check_pass(username, password)
+      false -> :no_user_found
+    end
+  end
+
+  defp user_exists(username), do: Database.get_by(:username, Account, username) != nil
+
+  defp check_pass(username, password) do
     hash_db = Database.get_by(:username, Account, username).password
     salt = Database.get_by(:username, Account, username).salt
+
     Auth.verify_pass(password, salt, hash_db)
   end
 
