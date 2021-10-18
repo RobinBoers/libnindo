6b53d3a21924b3c205c8fc28b1c87139cbfc438d
Robin Boers <robindev2019@outlook.com>
2021-10-18 17:16:44 +0200

    Added method to post repy

--- lib/nindo/posts/comments.ex
+++ lib/nindo/posts/comments.ex
@@ -12,6 +12,14 @@ defmodule Nindo.Comments do
     |> Database.put(Comment)
   end
 
+  def reply(comment_id, title, body, logged_in \\ logged_in())
+  def reply(_, _, _, false), do: {:error, "Not logged in. "}
+
+  def reply(comment_id, title, body, true) do
+    comment = get(comment_id)
+    new(comment.post_id, title, body)
+  end
+
   def get(id) do
     Database.get(Comment, id)
   end
