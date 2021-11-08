d6152e2cc776bea1c9482d04fb3a7595e71de103
Robin Boers <robindev2019@outlook.com>
2021-11-08 14:37:16 +0100

    Removed likes

--- lib/nindo/posts.ex
+++ lib/nindo/posts.ex
@@ -24,42 +24,4 @@ defmodule Nindo.Posts do
     Database.get_all(Post, limit)
   end
 
-  def like(id, logged_in \\ logged_in())
-  def like(_, false), do: {:error, "Not logged in. "}
-
-  def like(id, true) do
-    case is_liked(id) do
-      true -> :already_liked
-      false ->
-        change_like_count(id, 1)
-        change_liked(id, :add)
-    end
-  end
-
-  def dislike(id, logged_in \\ logged_in())
-  def dislike(_, false), do: {:error, "Not logged in. "}
-
-  def dislike(id, true) do
-    case is_liked(id) do
-      true ->
-        change_liked(id, :rem)
-        change_like_count(id, -1)
-      _ -> :not_liked
-    end
-  end
-
-  # Private methods
-
-  defp change_like_count(post_id, amount) do
-    post = get(post_id)
-    current_likes = post.like_count
-    Database.update(post, :like_count, current_likes + amount)
-  end
-
-  defp is_liked(id), do: Enum.member?(get_liked(user.id), id)
-  defp get_liked(user_id), do: Accounts.get(user_id).liked
-
-  defp change_liked(id, :add), do: Accounts.change(:liked, get_liked(user.id) ++ [id])
-  defp change_liked(id, :rem), do: Accounts.change(:liked, get_liked(user.id) -- [id])
-
 end
