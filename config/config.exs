418b9404d90c99966ebfa77f18324a51c9c4416d
Robin Boers <robindev2019@outlook.com>
2021-11-12 18:16:21 +0100

    Added cloak

--- config/config.exs
+++ config/config.exs
@@ -6,3 +6,13 @@ config :nin_db,
 config :nin_db, NinDB.Repo,
   database: "nindb",
   hostname: "localhost"
+
+config :nin_db, NinDB.Vault,
+  ciphers: [
+   default: {
+      Cloak.Ciphers.AES.GCM,
+      tag: "AES.GCM.V1",
+      key: Base.decode64!("goQI42SEuU17EvCwQ9jdlKxDeSecXF7s02wnNwkwuEI="),
+      iv_length: 12
+    }
+  ]
